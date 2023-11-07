38912a7319c3155ab8de1f8367f8fe110cb66ab0
saksham-nxt <saksham@nxt.pe>
2023-11-08 02:47:58 +0530

    Video adding and play functionality created

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,62 +1,103 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const VideoPlayer = () => {
   const canvasRef = useRef(null);
-  const [videoFile, setVideoFile] = useState(null);
-  const [duration, setDuration] = useState(null);
-  const [isPlaying, setIsPlaying] = useState(false);
   const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [showControls, setShowControls] = useState(true);
+
+  const fps = 60;
+  const width = 1280;
+  const height = 720;
+  let canvasInterval = null;
+
   const handleFileSelect = (event) => {
     const file = event.target.files[0];
-    setVideoFile(file);
-    const videoURL = URL.createObjectURL(file);
+
     if (videoRef.current) {
-      videoRef.current.src = videoURL;
-      videoRef.current.onloadedmetadata = () => {
-        setDuration(videoRef.current.duration);
-      };
+      videoRef.current.src = URL.createObjectURL(file);
+      videoRef.current.load();
     }
   };
-  const drawVideo = () => {
+
+  useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
 
-    if (videoRef.current && isPlaying) {
-      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-      requestAnimationFrame(drawVideo);
+    if (!canvas || !videoRef.current) return;
+
+    const drawImage = () => {
+      canvas.getContext("2d", { alpha: false }).drawImage(videoRef.current, 0, 0, width, height);
+    };
+
+    const startCanvasInterval = () => {
+      canvasInterval = window.setInterval(() => {
+        drawImage();
+      }, 1000 / fps);
+    };
+
+    videoRef.current.onplay = () => {
+      clearInterval(canvasInterval);
+      startCanvasInterval();
+      setShowControls(true);
+    };
+
+    videoRef.current.onpause = () => {
+      setShowControls(true);
+    };
+
+    videoRef.current.onended = () => {
+      setShowControls(true);
+    };
+
+    videoRef.current.onloadedmetadata = () => {
+      setDuration(videoRef.current.duration);
+    };
+
+    // Add event listener for the "canplay" event to start rendering when video is ready
+    videoRef.current.addEventListener("canplay", () => {
+      // Start rendering when video is ready
+      videoRef.current.play();
+      videoRef.current.pause();
+    });
+
+    return () => {
+      clearInterval(canvasInterval);
+    };
+  }, []);
+
+  const handlePlayPause = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
     }
   };
-  const handlePlayPause = () => {
-    if (videoFile) {
-      if (isPlaying) {
-        if (videoRef.current) {
-          videoRef.current.pause();
-        }
-      } else {
-        const videoURL = URL.createObjectURL(videoFile);
 
-        if (videoRef.current) {
-          videoRef.current.src = videoURL;
-          videoRef.current.onloadedmetadata = () => {
-            setDuration(videoRef.current.duration);
-            videoRef.current.play().then(() => {
-              setIsPlaying(true);
-              drawVideo();
-            }).catch(error => {
-              console.error('Video play error:', error);
-            });
-          };
-        }
-      }
-    }
+  const buttonStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    display: showControls ? "block" : "none",
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleFileSelect} />
-      {duration && <p>Duration: {duration.toFixed(2)} seconds</p>}
-      <canvas ref={canvasRef} width={500} height={500} style={{ border: "1px solid black" }} />
-      <button onClick={handlePlayPause}>{isPlaying ? "Pause" : "Play"}</button>
+    <div style={{ position: "relative" }}>
+      <input type="file" accept="video/*" onChange={handleFileSelect} />
+      <video ref={videoRef} width={width} height={height} style={{ display: "none" }} autoPlay={false}></video>
+      <canvas ref={canvasRef} width={width} height={height} style={{ border: "1px solid black" }}></canvas>
+      <div style={buttonStyle}>
+        <button onClick={handlePlayPause}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
+      {duration > 0 && (
+        <p>Duration: {duration.toFixed(2)} seconds</p>
+      )}
     </div>
   );
 };
